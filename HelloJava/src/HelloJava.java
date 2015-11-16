284cf716a33f3e0d2d316e0b12ca64fcad9863c4
YB.Ha <ybha72@gmail.com>
2015-11-16 14:03:44 +0900

    Add one more line

--- HelloJava/src/HelloJava.java
+++ HelloJava/src/HelloJava.java
@@ -3,5 +3,6 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
 		System.out.println("World's DanceHall");
+		System.out.println("Earth's DanceHall");
 	}
 }
